5616ac2ef9ce0e11cd9d9e905d6a74e0a9aa90d4
Bruno Gomes de Oliveira <bruno.oliveira234@fatec.sp.gov.br>
2024-03-23 18:05:00 -0300

    Teste Servidor

--- hydroflow/src/models/Login.php
+++ hydroflow/src/models/Login.php
@@ -27,6 +27,8 @@ class Login extends Model {
                 return $user;
             }
         }
+        $hash = password_hash($this->senha, PASSWORD_DEFAULT);
+        echo $hash;
         throw new AppException("Usuário e Senha inválidos.");
         
     }
